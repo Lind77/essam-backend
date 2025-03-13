0df81d5a71d7026b83f11c01b2fbcb50fe2f2aa5
digitechgrouperu <digitechgrouperu@gmail.com>
2025-03-12 19:56:16 -0500

    add login route

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://essam.fun:3000',
     credentials: true
 }))
 
