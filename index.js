84e280499c783ca8147de26e0ff36a0c2bead61c
digitechgrouperu <digitechgrouperu@gmail.com>
2025-03-12 19:26:40 -0500

    add login route

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 import express from 'express';
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const app = express();
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}))
 
 app.get('/', (req, res) =>{
     res.send('Hello World Lind');
 })
 
-app.get('/login', (req, res) =>{
-    console.log(req);
+app.post('/login', (req, res) =>{
+   const {email, password} = req.body;
+   console.log('Datos recibidos:', { email, password });
+   if (email === 'usuario@example.com' && password === 'contraseña') {
+     // Si las credenciales son válidas, enviar una respuesta exitosa
+     res.status(200).json({ message: 'Login exitoso' });
+   } else {
+     // Si las credenciales son inválidas, enviar un error
+     res.status(401).json({ message: 'Credenciales incorrectas' });
+   }
 })
 
 app.listen(4000, '0.0.0.0', (req, res) =>{
